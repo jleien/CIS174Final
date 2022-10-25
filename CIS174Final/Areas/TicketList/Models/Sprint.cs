58199d6726a93f2c1433f8e05716edc69d75d80e
DESKTOP-OQ0O92M\kajak <jleiendecker@dmacc.edu>
2022-10-25 17:28:49 -0500

    most of task 2 completed. application has bug where fields are not being seen on the add page and therefore tasks cannot be sent to database

---
+++ CIS174Final/Areas/TicketList/Models/Sprint.cs
@@ -0,0 +1,8 @@
+﻿namespace CIS174Final.Areas.TicketList.Models
+{
+    public class Sprint
+    {
+        public string SprintId { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ CIS174Final/Migrations/Ticket/20221025214119_InitialCreate.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CIS174Final.Migrations.Ticket
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sprints",
+                columns: table => new
+                {
+                    SprintId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sprints", x => x.SprintId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    StatusId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.StatusId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SprintId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    pointId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Statuses_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Statuses",
+                        principalColumn: "StatusId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Sprints",
+                columns: new[] { "SprintId", "Number" },
+                values: new object[,]
+                {
+                    { "1", "1" },
+                    { "2", "2" },
+                    { "3", "3" },
+                    { "4", "4" },
+                    { "5", "5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Statuses",
+                columns: new[] { "StatusId", "Name" },
+                values: new object[,]
+                {
+                    { "closed", "Completed" },
+                    { "progress", "In Progress" },
+                    { "qa", "Quality Assurance" },
+                    { "todo", "To-Do" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_StatusId",
+                table: "Tickets",
+                column: "StatusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sprints");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+        }
+    }
+}
