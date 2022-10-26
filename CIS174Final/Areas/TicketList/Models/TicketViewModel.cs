0203237a893bc96c547a0ddf20b507b302aeff9e
Alex <112102138+34ykri@users.noreply.github.com>
2022-10-26 12:48:37 -0500

    mostly fixed ticket need fix on ticket name, status, description. information is not being sent correctly

--- CIS174Final/Areas/TicketList/Models/TicketViewModel.cs
+++ CIS174Final/Areas/TicketList/Models/TicketViewModel.cs
@@ -4,17 +4,19 @@ namespace CIS174Final.Areas.TicketList.Models
 {
     public class TicketViewModel
     {
+        
         public TicketViewModel()
         {
             CurrentTask = new Ticket();
         }
-        public Filters Filters { get; set; }
+        
+        public Ticket CurrentTask { get; set; }  //used for Add
+       // public Filters Filters { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Sprint> Sprint { get; set; }
-        public int Point { get; set; }
-        public List<Status> Statuses { get; set; }
+        //public List<Sprint> Sprint { get; set; }
+        public int PointId { get; set; }
+       // public List<Status> Statuses { get; set; }
 
-        public Ticket CurrentTask { get; set; }  //used for Add
     }
 }
