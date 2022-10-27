94138b7be020c724410cf8fca47f28d630c47619
Alex <112102138+34ykri@users.noreply.github.com>
2022-10-26 22:07:59 -0500

    tickets description is being sent correctly now status and sprint is not being sent correctly to this day

--- CIS174Final/Areas/TicketList/Models/Ticket.cs
+++ CIS174Final/Areas/TicketList/Models/Ticket.cs
@@ -13,6 +13,7 @@ namespace CIS174Final.Areas.TicketList.Models
         public string Description { get; set; }
         [Required(ErrorMessage = "Please enter a sprint number.")]
         public string SprintId { get; set; }
+        public Sprint Sprint { get; set; }
         [Required(ErrorMessage = "Please enter a point value.")]
         public string pointId { get; set; }
         [Required(ErrorMessage = "Please select a status.")]
