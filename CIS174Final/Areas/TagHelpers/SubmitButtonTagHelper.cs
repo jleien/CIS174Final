9e5f604b683d34ea3a5e4e66b4d46c619f0371c8
DESKTOP-OQ0O92M\kajak <jleiendecker@dmacc.edu>
2022-11-23 14:50:58 -0600

    taghelper for submit button created

---
+++ CIS174Final/Areas/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace CIS174Final.Areas.TagHelpers
+{
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("type", "submit");
+
+            string newClasses = "btn btn-primary";
+            string oldClasses = output.Attributes["class"]?.Value?.ToString();
+            string classes = (string.IsNullOrEmpty(oldClasses)) ? newClasses : $"{oldClasses} {newClasses}";
+            output.Attributes.SetAttribute("class", classes);
+        }
+    }
+}
